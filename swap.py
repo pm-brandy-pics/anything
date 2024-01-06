8287fae73e53c14203256b53a059ee784d421c17
Sneha Patnaik <contactsnehapatnaik@gmail.com>
2024-01-06 22:25:28 +0530

    initial commit

---
+++ swap.py
@@ -0,0 +1,9 @@
+a=input('Enter the value of first variable a:')
+b=input('Enter the value of second variable b:')
+print('Before swapping:')
+print('a=',a,'b=',b)
+c=a
+a=b
+b=c
+print('After swapping:')
+print('a=',a,'b=',b)
