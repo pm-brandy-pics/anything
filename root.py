9b3ac73ec7ceac94ca2bff39de070edd006da7fb
Sneha Patnaik <contactsnehapatnaik@gmail.com>
2024-01-06 22:29:52 +0530

    adding root.py

---
+++ root.py
@@ -0,0 +1,7 @@
+a=float(input('Enter value for a'))
+b=float(input('Enter value for b'))
+c=float(input('Enter value for c'))
+d=(b*b)-(4*a*c)
+root1=(-b-(d**0.5))/(2*a)
+root2=(-b+(d**0.5))/(2*a)
+print('Root1 {0} Root2 {1}'.format(root1,root2))
